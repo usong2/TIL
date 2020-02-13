b41ffbb1e4ec8197e12e4f5a019f67f78a252e71
usong2 <dbthd6@naver.com>
2020-02-13 22:44:00 +0900

    function

---
+++ docs/Node.js/01_Node.js_Programming/Node_Example/ch03_test1.js
@@ -0,0 +1,5 @@
+var age = 28;
+console.log("나이 : %d", age);
+
+var name = "윳홍이";
+console.log("이름 : %s", name);
---
+++ docs/Node.js/01_Node.js_Programming/Node_Example/ch03_test2.js
@@ -0,0 +1,9 @@
+let Person = [];
+
+Person["age"] = 28;
+Person["name"] = "윳홍이";
+Person.mobile = "010-9999-9999";
+
+console.log("나이 : %d", Person.age);
+console.log("이름 : %s", Person.name);
+console.log("전화 : %s", Person.mobile);
